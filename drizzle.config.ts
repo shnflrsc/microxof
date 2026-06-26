483a99adff70d8465bb4fbbc127999139cd192b8
Darl Shanen Floresca <floresca.shanegarcia@gmail.com>
2026-06-27 00:08:03 +0800

    Hard code env variables for now

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
 
-const dbName = process.env.POSTGRES_DB || 'local';
-const dbUser = process.env.POSTGRES_USER || 'root';
-const dbPassword = process.env.POSTGRES_PASSWORD || 'password';
-
-const dbUrl = `postgres://${dbUser}:${dbPassword}@db:5432/${dbName}`;
-
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
-	dialect: 'postgresql',
-	dbCredentials: { url: dbUrl },
-	verbose: true,
-	strict: true
+    schema: './src/lib/server/db/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: { url: "postgres://root:mysecretpassword@db:5432/local" },
+    verbose: true,
+    strict: true
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,12 +3,6 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-const dbName = env.POSTGRES_DB || 'local';
-const dbUser = env.POSTGRES_USER || 'root';
-const dbPassword = env.POSTGRES_PASSWORD || 'password';
-
-const dbUrl = `postgres://${dbUser}:${dbPassword}@db:5432/${dbName}`;
-
-const client = postgres(dbUrl);
+const client = postgres("postgres://root:mysecretpassword@db:5432/local");
 
 export const db = drizzle(client, { schema });
